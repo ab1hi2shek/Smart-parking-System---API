a9798305007794177d45a81ab8490f5247a3a20f
ab1hi2shek <ab1hi2shek@gmail.com>
2018-03-15 16:43:46 +0530

    added nodemon and error handler

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const parkingRoutes = require('./api/routes/parkings');
 
+app.use(morgan('dev'));
+
+//Routes handling requests
 app.use('/parkings', parkingRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Page not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
 
 module.exports = app;
