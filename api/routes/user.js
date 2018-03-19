6498da55b0026d0020519e09018b4dbf18ae665a
ab1hi2shek <ab1hi2shek@gmail.com>
2018-03-20 03:35:01 +0530

    changed back from get to post

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,7 +7,7 @@ router.get("/", userController.users_get_all);
 
 router.post("/signup", userController.signup_user);
 
-router.get("/login", userController.login_user);
+router.post("/login", userController.login_user);
 
 router.delete("/:userId", userController.delete_user);
 
